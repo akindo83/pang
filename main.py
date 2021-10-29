7dc966b027f3f3c291acccf69d6db989d0f366b2
akindo83 <akindo83@gmail.com>
2021-10-30 01:39:40 +0900

    main

---
+++ main.py
@@ -0,0 +1,29 @@
+import pygame
+
+pygame.init()
+
+player_group = pygame.sprite.RenderUpdates()
+missile_group = pygame.sprite.RenderUpdates()
+ball_group = pygame.sprite.RenderUpdates()
+
+#player = Player(player_group)
+
+screen = pygame.display.set_mode((1024, 768))
+pygame.display.set_caption("팡팡")
+
+clock = pygame.time.Clock();
+
+#background = pygame.image.load("background.png")
+
+is_playing = True
+
+while is_playing :
+    for event in pygame.event.get() :
+        if event.type in (pygame.QUIT, pygame.KEYDOWN) :
+            is_playing = False
+
+    #screen.blit(background, (0, 0))
+    pygame.display.update()
+    clock.tick(60)
+
+pygame.quit()
